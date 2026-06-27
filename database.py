9b267ba4471b89b86652b48319bceee93dc982ca
dliciagourmetoficial-crypto <dliciagourmet.oficial@gmail.com>
2026-06-26 23:27:33 -0500

    Update database.py

--- database.py
+++ database.py
@@ -8,3 +8,15 @@ def init_db():
                       (admin_id INTEGER, client_id INTEGER, client_name TEXT)''')
     conn.commit()
     conn.close()
+
+def eliminar_de_sheets(chat_id):
+    # Buscamos la fila que coincida con el ID
+    cell = worksheet.find(str(chat_id))
+    if cell:
+        worksheet.delete_rows(cell.row)
+        return True
+    return False
+
+def obtener_todos_los_pedidos():
+    # Devuelve todos los registros para que el admin los vea
+    return worksheet.get_all_records()
