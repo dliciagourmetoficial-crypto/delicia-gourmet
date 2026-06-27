d3e0cd7d2fa38a699f0472adfc8bcc92bc6648b7
dliciagourmetoficial-crypto <dliciagourmet.oficial@gmail.com>
2026-06-27 16:07:34 -0500

    Update database.py

--- database.py
+++ database.py
@@ -8,6 +8,14 @@ def init_db():
                       (admin_id INTEGER, client_id INTEGER, client_name TEXT)''')
     conn.commit()
     conn.close()
+    
+def guardar_chat_activo(admin_id, client_id, client_name):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute("INSERT OR REPLACE INTO active_chats (admin_id, client_id, client_name) VALUES (?, ?, ?)", 
+                   (admin_id, client_id, client_name))
+    conn.commit()
+    conn.close()
 
 def eliminar_de_sheets(chat_id):
     # Buscamos la fila que coincida con el ID
