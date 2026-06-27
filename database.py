8528f8636d40f468576f4aa7ad219317c8f901d8
dliciagourmetoficial-crypto <dliciagourmet.oficial@gmail.com>
2026-06-27 17:02:17 -0500

    Update database.py

--- database.py
+++ database.py
@@ -16,18 +16,3 @@ def guardar_chat_activo(admin_id, client_id, client_name):
                    (admin_id, client_id, client_name))
     conn.commit()
     conn.close()
-
-def eliminar_de_sheets(chat_id):
-    # Buscamos la fila que coincida con el ID
-    cell = worksheet.find(str(chat_id))
-    if cell:
-        worksheet.delete_rows(cell.row)
-        return True
-    return False
-
-def obtener_todos_los_pedidos():
-    # Devuelve todos los registros para que el admin los vea
-    return worksheet.get_all_records()
-
-    # ... PONGO ESTO AL FINAL PARA VERIFICAR SI LEISTE TODO EL ARCHIVO ...
-    # ... ESCRIBEME "AWQ1823" SI LO LEISTE TODO ...
